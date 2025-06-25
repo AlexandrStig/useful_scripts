21b05e087fedbd720065da0ab141eb811d92d763
AlexandrStig <73491857+AlexandrStig@users.noreply.github.com>
2025-06-25 12:15:34 +0300

    Update dba_tools_pg.sql
    ADD show settings on single database

--- PostgreSQL/dba_tools_pg.sql
+++ PostgreSQL/dba_tools_pg.sql
@@ -8,8 +8,8 @@ SELECT pg_size_pretty(pg_total_relation_size('"public"."lid_subscription"'));
 -- column size
 SELECT pg_size_pretty(sum(pg_column_size(column_name))) FROM table_name;
 
--- список установленных расширений (EXTENSIONS) с версиям
-select * from pg_available_extensions where installed_version is not null; 		                                    и
+-- список установленных расширений (EXTENSIONS) с версиями
+select * from pg_available_extensions where installed_version is not null;
 
 -- просмотр статистики по полю column_name в таблице table_name
 select * from pg_stats where tablename = 'table_name' and attname = 'column_name';		
@@ -23,3 +23,17 @@ select * from pg_indexes where tablename='table_name';
 -- частота использования индекса idx_name
 select indexrelname, idx_scan from pg_stat_user_indexes where indexrelname= 'idx_name';
 
+-- просмотреть установленные конфиги для определенной БД
+SELECT d.datname, s.setconfig 
+FROM pg_database d
+LEFT JOIN pg_db_role_setting s ON d.oid = s.setdatabase
+WHERE d.datname = 'test';
+
+-- или так
+SELECT datname, unnest(setconfig) AS setting
+FROM pg_database d
+LEFT JOIN pg_db_role_setting s ON d.oid = s.setdatabase
+WHERE datname = 'test' AND setconfig IS NOT NULL;
+
+-- или так
+psql -d test -c "SHOW config_name;"
