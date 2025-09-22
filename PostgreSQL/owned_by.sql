3c7523e6395e5509b29fe19f10f4db5d71b265b9
AlexandrStig <73491857+AlexandrStig@users.noreply.github.com>
2025-09-22 16:47:45 +0300

    Update owned_by.sql

--- PostgreSQL/owned_by.sql
+++ PostgreSQL/owned_by.sql
@@ -1,11 +1,15 @@
--- Show tables and sequences owned by user
-select c.relname, relkind
+-- Show tables and sequences owned by user with schema information
+select 
+    n.nspname as schema_name,
+    c.relname, 
+    c.relkind
 from
     pg_class c
-    inner join
-    pg_roles r on r.oid = c.relowner
+    inner join pg_roles r on r.oid = c.relowner
+    inner join pg_namespace n on n.oid = c.relnamespace
 where
-    r.rolname = 'aaevstegneev'
-     and
-    c.relkind in ('r', 'S')
-order by c.relname ;
+    r.rolname = 'users-and-locations'
+    and c.relkind in ('r', 'S')
+order by 
+    n.nspname,
+    c.relname ;
