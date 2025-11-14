a22f1a78a966772a9f1f7b7314146d93f8d9149d
Alexandr Evstegneev <73491857+AlexandrStig@users.noreply.github.com>
2025-11-15 00:08:16 +0300

    add comment backends_num_detail.sql

--- PostgreSQL/backends_num_detail.sql
+++ PostgreSQL/backends_num_detail.sql
@@ -1,4 +1,6 @@
 -- Информация о подключенных клиентах (клиенты сгруппированы по адресу подключения, имени пользователя и имени БД)
+-- client_addr=NULL - подключение выполнено через UNIX-сокет с того же узла, где запущен сервер СУБД
+-- usename и datname = NULL - фоновые службы PostgreSQL
 SELECT client_addr, usename, datname, count(*)
 FROM pg_stat_activity
 GROUP BY 1,2,3
