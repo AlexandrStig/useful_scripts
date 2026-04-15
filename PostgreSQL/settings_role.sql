5e1d0c77f2a491823ea116bc987c714e2728149e
Alexandr Evstegneev <73491857+AlexandrStig@users.noreply.github.com>
2026-04-15 13:13:12 +0300

    edit

--- PostgreSQL/settings_role.sql
+++ PostgreSQL/settings_role.sql
@@ -6,6 +6,6 @@ SELECT
 FROM pg_db_role_setting s
 LEFT JOIN pg_database d ON s.setdatabase = d.oid
 JOIN pg_roles r ON s.setrole = r.oid
-WHERE r.rolname = 'aaevstegneev'
+WHERE r.rolname = 'user_name'
   AND setconfig IS NOT NULL
 ORDER BY database_name;
--- PostgreSQL/short_queries.sql
+++ PostgreSQL/short_queries.sql
@@ -9,23 +9,7 @@ select * from pg_indexes where tablename='table_name';
 
 -- частота использования индекса idx_name
 select indexrelname, idx_scan from pg_stat_user_indexes where indexrelname= 'idx_name';
-
--- просмотреть установленные конфиги для определенной БД
-SELECT d.datname, s.setconfig 
-FROM pg_database d
-LEFT JOIN pg_db_role_setting s ON d.oid = s.setdatabase
-WHERE d.datname = 'db_name'; -- or current_database()
-
--- или так
-SELECT datname, unnest(setconfig) AS setting
-FROM pg_database d
-LEFT JOIN pg_db_role_setting s ON d.oid = s.setdatabase
-WHERE datname = 'db_name' AND setconfig IS NOT NULL; -- or current_database()
-
--- или так
-psql -d test -c "SHOW config_name;"
-
-  
+ 
 -- Долгие транзакции (>5 минут)
 SELECT pid, now() - xact_start AS duration, query 
 FROM pg_stat_activity 
