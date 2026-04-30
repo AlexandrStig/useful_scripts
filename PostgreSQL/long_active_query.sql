ce0466369fe92eb46b82632a2fd68f563a5db014
Alexandr Evstegneev <73491857+AlexandrStig@users.noreply.github.com>
2026-04-30 12:43:39 +0300

    now() -> clock_timestamp()

--- PostgreSQL/long_active_query.sql
+++ PostgreSQL/long_active_query.sql
@@ -10,11 +10,11 @@
     wait_event_type,
     wait_event,
     query,
-    now() - query_start AS duration
+    clock_timestamp() - query_start AS duration
 FROM
     pg_stat_activity
 WHERE
     state = 'active'
-    AND now() - query_start > interval '1 minute' -- Adjust the duration as needed
+    AND clock_timestamp() - query_start > interval '1 minute' -- Adjust the duration as needed
 ORDER BY
     duration DESC \gx
